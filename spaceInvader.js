ac70bc268e63051693a738ec100376f000e63d47
SafaeNajjari <113999511+SafaeNajjari@users.noreply.github.com>
2024-09-29 16:07:34 +0200

    Avatar done

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,33 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawAvatar();
+
+function drawAvatar() {
+
+    let canvas = document.querySelector('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.rect(25, 75, 300, 300);
+    context.fillStyle = "black"
+    context.fill();
+
+    context.beginPath();
+    context.rect(100, 100, 50, 50);
+    context.rect(100, 150, 50, 50);
+    context.rect(150, 150, 50, 50);
+    context.rect(200, 100, 50, 50);
+    context.rect(200, 200, 50, 50);
+    context.rect(200, 250, 50, 50);
+    context.rect(150, 250, 50, 50);
+    context.rect(200, 150, 50, 50);
+    context.rect(100, 200, 50, 50);
+    context.rect(100, 250, 50, 50);
+    context.rect(150, 300, 50, 50);
+    context.fillStyle = "green"
+    context.stroke();
+    context.fill();
+
+}
